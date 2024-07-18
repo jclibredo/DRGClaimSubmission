77e3656e74f758ebd5a3912162ef8b263f1cfb3b
MinoSun <123798384+jclibredo@users.noreply.github.com>
2024-07-18 18:46:55 +0800

    g

--- src/main/java/drg/drgclaimsubmission/structures/EncryptedXML.java
+++ src/main/java/drg/drgclaimsubmission/structures/EncryptedXML.java
@@ -5,69 +5,30 @@
  */
 package drg.drgclaimsubmission.structures;
 
+import lombok.Data;
+
 /**
  *
  * @author MinoSun
  */
+@Data
 public class EncryptedXML {
     
     public EncryptedXML() {
     }
-
     private String docMimeType;
     private String hash;
     private String key1;
     private String key2;
     private String iv;
     private String doc;
-
-    public String getDocMimeType() {
-        return docMimeType;
-    }
-
-    public void setDocMimeType(String docMimeType) {
-        this.docMimeType = docMimeType;
-    }
-
-    public String getHash() {
-        return hash;
-    }
-
-    public void setHash(String hash) {
-        this.hash = hash;
-    }
-
-    public String getKey1() {
-        return key1;
-    }
-
-    public void setKey1(String key1) {
-        this.key1 = key1;
-    }
-
-    public String getKey2() {
-        return key2;
-    }
-
-    public void setKey2(String key2) {
-        this.key2 = key2;
-    }
-
-    public String getIv() {
-        return iv;
-    }
-
-    public void setIv(String iv) {
-        this.iv = iv;
-    }
-
-    public String getDoc() {
-        return doc;
-    }
-
-    public void setDoc(String doc) {
-        this.doc = doc;
-    }
-    
-    
 }
+
+
+
+//text = "Hello";
+//   text = kalsdjfoq3u4pufgpqohdlksldkhf
+
+
+//  CF5.xml
+
